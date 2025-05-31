5eff8e7244cd287bf8d9482d4ad33cca20f8b9c9
kevanth <kevantheo.halim@gmail.com>
2025-06-01 03:46:18 +1000

    added css color themes

--- stocktake/src/app/item.tsx
+++ stocktake/src/app/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+
 interface ItemProps {
 	name: string;
 	initialCount: number;
@@ -7,12 +8,22 @@ interface ItemProps {
 
 export default function Item({name, initialCount}:ItemProps) {
   const [count,setCount] = useState(initialCount)
-  const buttonClass = "border-1 hover:bg-blue-500"
+  const buttonClass = "border-1 border-border hover:bg-border aspect-square w-8"
   
   return (
-    <div className="flex flex-col items-center border border-gray-400 p-4 rounded">
+    <div className="flex flex-col items-center border text-textPrimary border-border p-4 rounded bg-secondary">
         <p className="text-lg font-bold">{name}</p>
-        <p className="text-sm text-gray-700">Count: {count}</p>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => {
+            const val = e.target.value
+            if (/^\d*$/.test(val)) {
+              setCount(Number(val))
+            }
+          }}
+          className="border px-2 py-1 my2 rounded "
+        />
       <div className="flex gap-2">
         <button className={buttonClass} onClick={()=>{setCount((count == 0)?0:count - 1)}}>-</button>
         <button className={buttonClass} onClick={()=>{setCount(count + 1)}}>+</button>
--- stocktake/src/app/page.tsx
+++ stocktake/src/app/page.tsx
@@ -2,10 +2,11 @@ import Item from "./item";
 
 export default function Home() {
   return (
-    <div className="flex" >
+    <div className="grid grid-cols-1 md:grid-cols-3 bg-background" >
       <Item name="tissue" initialCount={1}></Item>
       <Item name="abc" initialCount={1}></Item>
+      <Item name="abc" initialCount={1}></Item>
+      <Item name="abc" initialCount={1}></Item>
     </div>
-    
   );
 }
