2d15b81d904ed1bd79a9cba656a47d3194f90899
kevanth <kevantheo.halim@gmail.com>
2025-06-01 03:54:33 +1000

    configured layout of items cotainer

--- stocktake/src/app/item.tsx
+++ stocktake/src/app/item.tsx
@@ -11,7 +11,7 @@ export default function Item({name, initialCount}:ItemProps) {
   const buttonClass = "border-1 border-border hover:bg-border aspect-square w-8"
   
   return (
-    <div className="flex flex-col items-center border text-textPrimary border-border p-4 rounded bg-secondary">
+    <div className="flex flex-col items-center border text-textPrimary border-border p-4 rounded bg-secondary aspect-square">
         <p className="text-lg font-bold">{name}</p>
         <input
           type="text"
--- stocktake/src/app/page.tsx
+++ stocktake/src/app/page.tsx
@@ -2,7 +2,7 @@ import Item from "./item";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 bg-background" >
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-[10%] bg-background w-[80%] mx-auto mt-10" >
       <Item name="tissue" initialCount={1}></Item>
       <Item name="abc" initialCount={1}></Item>
       <Item name="abc" initialCount={1}></Item>
