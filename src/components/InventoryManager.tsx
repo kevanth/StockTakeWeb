d4f91969bdda9831b6d30c6e3e4c8e3569526aca
kevanth <kevantheo.halim@gmail.com>
2025-10-06 21:27:44 +1100

    feature: set filter to dropdownmenu shadcn

--- src/components/InventoryManager.tsx
+++ src/components/InventoryManager.tsx
@@ -20,6 +20,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { set } from "lodash";
 import { ListFilter } from "lucide-react";
 
@@ -113,9 +121,19 @@ export function InventoryManager() {
             onChange={(e) => setSearchItem(e.target.value)}
             className="w-full"
           ></Input>
-          <Button>
-            <ListFilter onClick={() => setSearchItem("")} />
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <ListFilter />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         {}
         {/* Items List */}
